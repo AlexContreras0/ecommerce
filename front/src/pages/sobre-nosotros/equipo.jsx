9076399fae3993005801d99aaad9a7f7c9608b1c
AlexContreras0 <alexcontrerasg98@gmail.com>
2024-03-08 18:14:32 +0100

    cambios en sobre nosotros y equipo

--- front/src/pages/sobre-nosotros/equipo.jsx
+++ front/src/pages/sobre-nosotros/equipo.jsx
@@ -1,10 +1,63 @@
+import FooterComponent from "@/components/FooterComponent/FooterComponent";
 import NavMenu from "@/components/NavMenu/NavMenu";
 import React from "react";
+import styles from "../../styles/equipo.module.css";
 
 export default function page() {
   return (
     <div>
       <NavMenu />
+      <div className={styles.container}>
+        <h1 className={styles.title}>Conóce a nuestro equipo</h1>
+        <div className={styles.miembroContainer}>
+          <div className={styles.miembroCard}>
+            {/* <img
+              src="https://c.tenor.com/A3PC_hXmTs4AAAAC/tenor.gif"
+              class="gif"
+              width="250px"
+              height="300px"
+              alt="gif"
+            /> */}
+            <h2>Nombre Apellido Apellido</h2>
+            <h3>Cargo</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero,
+              labore?
+            </p>
+          </div>
+          <div className={styles.miembroCard}>
+            {/* <img
+              src="https://c.tenor.com/A3PC_hXmTs4AAAAC/tenor.gif"
+              class="gif"
+              width="250px"
+              height="300px"
+              alt="gif"
+            /> */}
+            <h2>Nombre Apellido Apellido</h2>
+            <h3>Cargo</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero,
+              labore?
+            </p>
+          </div>
+          <div className={styles.miembroCard}>
+            {/* <img
+              src="https://c.tenor.com/A3PC_hXmTs4AAAAC/tenor.gif"
+              class="gif"
+              width="250px"
+              height="300px"
+              alt="gif"
+            /> */}
+            <h2>Nombre Apellido Apellido</h2>
+            <h3>Cargo</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero,
+              labore?
+            </p>
+          </div>
+        </div>
+      </div>
+      <FooterComponent />
     </div>
   );
 }
