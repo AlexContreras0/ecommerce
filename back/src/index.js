fab3ccb752defa6e2c220514cb27c5758eba4e0f
Saramalagamba <saramalagamba@gmail.com>
2024-03-09 22:44:36 +0100

    He intentado solucionar el bloqueo de CORS y no he podido

--- back/src/index.js
+++ back/src/index.js
@@ -32,7 +32,7 @@ db.on("disconnected", () => {
 })
 
 app.use(express.json())
-app.use(cors({origin: 'http://localhost:3000'}))
+app.use(cors({origin: "http://localhost:3000"}))
 
 app.use('/products', productsRouter)
 app.use('/favs', favsRouter)
